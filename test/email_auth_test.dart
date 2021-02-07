b2ca6356080d943bd381afef27c7f827ab4d009c
GJJ2019 <gauravjajoo98@gmail.com>
2021-02-07 22:05:11 +0530

    - meta package import - added required keyword for sendOtp & validate method - change recieverMail to receiverMail (typo in name)

--- test/email_auth_test.dart
+++ test/email_auth_test.dart
@@ -1,9 +1,9 @@
 // import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:email_auth/email_auth.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('check failing validation', () {
-    expect(EmailAuth.validate(recieverMail: "sample", userOTP: "15252"), false);
+    expect(EmailAuth.validate(receiverMail: "sample", userOTP: "15252"), false);
   });
 }
