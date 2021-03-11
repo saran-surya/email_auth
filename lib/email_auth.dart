70b750ce9a2c70e6098d23686fd1722c09d535f7
GJJ2019 <gauravjajoo98@gmail.com>
2021-03-11 10:04:08 +0530

    - upgrading http package - migrating to null safety

--- lib/email_auth.dart
+++ lib/email_auth.dart
@@ -1,33 +1,35 @@
 import 'dart:async';
 import 'dart:convert' as convert;
+
 import 'package:http/http.dart' as http;
-import 'package:flutter/material.dart';
 
 class EmailAuth {
   /// A class to handle the operations
   ///
   /// Assign your session Name / Company name here using the static method
-  static String sessionName;
-  static String _finalOTP;
-  static String _finalEmail;
+  static late String sessionName;
+  static late String _finalOTP;
+  static late String _finalEmail;
 
   static bool _isEmail(String email) {
-    String p =
-        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+    String p = r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
     RegExp regExp = new RegExp(p);
     print(regExp.hasMatch(email));
     return regExp.hasMatch(email);
   }
 
   ///This functions returns a future of boolean stating if the OTP was sent.
-  static Future<bool> sendOtp({@required String receiverMail}) async {
+  static Future<bool> sendOtp({required String receiverMail}) async {
     try {
       if (!_isEmail(receiverMail)) {
         print("email ID is not valid");
         return false;
       }
       http.Response _response = await http.get(
-          "https://app-authenticator.herokuapp.com/auth/${receiverMail.toLowerCase()}?CompanyName=$sessionName");
+        Uri.parse(
+          "https://app-authenticator.herokuapp.com/auth/${receiverMail.toLowerCase()}?CompanyName=$sessionName",
+        ),
+      );
       Map<String, dynamic> _data = convert.jsonDecode(_response.body);
       if (_data["success"]) {
         _finalEmail = receiverMail;
@@ -37,19 +39,15 @@ class EmailAuth {
         return false;
       }
     } catch (e) {
-      print(e.message);
+      print(e);
       return false;
     }
   }
 
   ///This functions returns a future of boolean stating if the user provided data is correct
-  static bool validate({
-    @required String receiverMail,
-    @required String userOTP,
-  }) {
+  static bool validate({required String receiverMail, required String userOTP}) {
     if (_finalEmail.length > 0 && _finalOTP.length > 0) {
-      if (receiverMail.trim() == _finalEmail.trim() &&
-          userOTP.trim() == _finalOTP.trim()) {
+      if (receiverMail.trim() == _finalEmail.trim() && userOTP.trim() == _finalOTP.trim()) {
         print("Validation success");
         return true;
       } else {
